966d490eac99f2465d537a02222d845c0895a21b
HAI-PC\HAI <mjnhaj95@gmail.com>
2016-10-27 13:53:11 +0700

    service in angularjs

--- CDGShop.Web/Scripts/spa/app.js
+++ CDGShop.Web/Scripts/spa/app.js
@@ -3,17 +3,26 @@
 var myApp = angular.module('myModule', []);
 
 myApp.controller("schoolController", schoolController);
-myApp.controller("studentController", studentController);
-myApp.controller("teacherController", teacherController);
+myApp.service('Validator', Validator);
 
-function schoolController($scope) {
-    $scope.message = "Announcenment form school";
-}
-//declare
-function studentController($scope) {
-    $scope.message = "This is my message from student";
+schoolController.$inject = ['$scope', 'Validator'];
+
+function schoolController($scope, Validator) {
+    $scope.checkNumber = function () {
+        $scope.message = Validator.checkNumber(1);
+    }
+    $scope.num = 1;
 }
 
-function teacherController($scope) {
-    $scope.message = "This is my message from teacher";
+function Validator($window) {
+    return { checkNumber: checkNumber }
+
+    function checkNumber(input) {
+        if (input % 2 == 0) {
+            return 'This is even';
+        }
+        else {
+            return 'This is odd';
+        }
+    }
 }
