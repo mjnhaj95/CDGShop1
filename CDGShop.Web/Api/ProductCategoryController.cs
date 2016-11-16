c9c1ae703edfe4ef965de9799468be2c49009f6e
HAI-PC\HAI <mjnhaj95@gmail.com>
2016-11-16 16:02:43 +0700

    paging for list

--- CDGShop.Web/Api/ProductCategoryController.cs
+++ CDGShop.Web/Api/ProductCategoryController.cs
@@ -18,19 +18,32 @@ namespace CDGShop.Web.Api
         private IProductCategoryService _productCategoryService;
 
         public ProductCategoryController(IErrorService errorService, IProductCategoryService productCategoryService)
-                : base(errorService)
+            : base(errorService)
         {
             this._productCategoryService = productCategoryService;
         }
 
         [Route("getall")]
-        public HttpResponseMessage GetAll(HttpRequestMessage request)
+        public HttpResponseMessage GetAll(HttpRequestMessage request, int page, int pageSize = 20)
         {
             return CreateHttpResponse(request, () =>
             {
+                int totalRow = 0;
                 var model = _productCategoryService.GetAll();
-                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
-                var response = request.CreateResponse(HttpStatusCode.OK, responseData);
+
+                totalRow = model.Count();                              // lấy toàn bộ bản ghi gán vào totalRow
+                var query = model.OrderByDescending(x => x.CreatedDate).Skip(page * pageSize).Take(pageSize);
+
+                var responseData = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(query);
+
+                var paginationSet = new PaginationSet<ProductCategoryViewModel>()
+                {
+                    Items = responseData,
+                    Page = page,
+                    TotalCount = totalRow,
+                    TotalPages = (int)Math.Ceiling((decimal)totalRow / pageSize)     // Math.Ceiling làm tròn lên
+                };
+                var response = request.CreateResponse(HttpStatusCode.OK, paginationSet);
                 return response;
             });
         }
---
+++ CDGShop.Web/Infrastructure/Core/PaginationSet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace CDGShop.Web.Infrastructure.Core
+{
+    public class PaginationSet<T>
+    {
+        public int Page { set; get; } //
+
+        public int Count                        //
+        {
+            get                                 // Chỉ get
+            {
+                return (Items != null) ? Items.Count() : 0;
+            }
+        }
+
+        public int TotalPages { set; get; }         // Số trang
+        public int TotalCount { set; get; }         // Tổng số bản ghi
+        public IEnumerable<T> Items { set; get; }   // Lưu Items
+    }
+}
