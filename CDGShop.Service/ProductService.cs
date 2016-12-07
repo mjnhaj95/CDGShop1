1b8499994be68dcf4db3f972ece9c37391c0b176
HAI-PC\HAI <mjnhaj95@gmail.com>
2016-12-07 17:59:48 +0700

    sort list product

--- CDGShop.Service/ProductService.cs
+++ CDGShop.Service/ProductService.cs
@@ -24,7 +24,7 @@ namespace CDGShop.Service
 
         IEnumerable<Product> GetHotProduct(int top);
 
-        IEnumerable<Product> GetListProductByCategoryIdPaging(int categoryId, int page, int pageSize, out int totalRow);
+        IEnumerable<Product> GetListProductByCategoryIdPaging(int categoryId, int page, int pageSize, string sort, out int totalRow);
 
         Product GetById(int id);
 
@@ -140,9 +140,28 @@ namespace CDGShop.Service
             return _productRepository.GetMulti(x => x.Status && x.HotFlag == true).OrderByDescending(x => x.CreatedDate).Take(top);
         }
 
-        public IEnumerable<Product> GetListProductByCategoryIdPaging(int categoryId, int page, int pageSize, out int totalRow)
+        public IEnumerable<Product> GetListProductByCategoryIdPaging(int categoryId, int page, int pageSize, string sort, out int totalRow)
         {
             var query = _productRepository.GetMulti(x => x.Status && x.CategoryID == categoryId);
+
+            switch (sort)
+            {
+                case "popular":
+                    query = query.OrderByDescending(x => x.ViewCount);
+                    break;
+
+                case "discount":
+                    query = query.OrderByDescending(x => x.PromotionPrice.HasValue);
+                    break;
+
+                case "price":
+                    query = query.OrderByDescending(x => x.Price);
+                    break;
+
+                default:
+                    query = query.OrderByDescending(x => x.CreatedDate);
+                    break;
+            }
 
             totalRow = query.Count();
 
